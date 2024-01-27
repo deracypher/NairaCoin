1bb0b67be18ca5286355cdf34e4faf2fcef6da4c
deracypher <megachidox@gmail.com>
2024-01-27 15:41:02 +0100

    Added HeroSectionand Footer

---
+++ README.md
@@ -0,0 +1,31 @@
+# NairaCoin - Cryptocurrency Web App
+
+Web landing page of cryptocurrency site, built with React and Tailwindcss.
+
+
+
+## How to start the web
+
+```bash
+yarn
+yarn dev
+```
+
+## Build the project
+
+```bash
+yarn build
+```
+
+## Run a testing
+
+```bash
+yarn test
+```
+
+## Contributor
+- Deracypher
+
+## License
+
+Source code is under MIT license
---
+++ src/components/Layout/index.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+import Footer from '../../components/Footer';
+import Navbar from '../../components/Navbar';
+
+export default function Layout({ children }) {
+  return (
+    <>
+      <header>
+        <Navbar />
+      </header>
+      <main className='overflow-hidden'>
+        {children}
+      </main>
+      <Footer />
+    </>
+  );
+}
+
+Layout.propTypes = {
+  children: PropTypes.node
+};
