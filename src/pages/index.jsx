538e6617161e0b91305ef59d71c5836dd7dd7f18
deracypher <megachidox@gmail.com>
2024-01-27 16:02:28 +0100

    Added CryptocurrencySection and BuyAndTradeSection

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,13 +1,18 @@
 import React from 'react';
 
 import Layout from '../components/Layout';
+import CryptocurrencySection from '../components/sections/CryptocurrencySection';
 import HeroSection from '../components/sections/HeroSection';
+import BuyAndTradeSection from '../components/sections/BuyAndTradeSection';
 
 
 export default function index() {
   return (
     <Layout>
       <HeroSection />
+      <CryptocurrencySection />
+      <BuyAndTradeSection />
+      
     </Layout>
   );
 }
